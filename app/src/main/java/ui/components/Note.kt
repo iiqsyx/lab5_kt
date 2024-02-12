1980995d2bf1a165a0bdec520289348e5b154531
iiqsyx <alex_iiqsyx@mail.ru>
2024-02-12 17:37:21 +0300

    featback: NoteColor added to Note

--- app/src/main/java/ui/components/Note.kt
+++ app/src/main/java/ui/components/Note.kt
@@ -4,33 +4,40 @@ import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 import com.topic2.android.notes.theme.rwGreen
 
 
 @Composable
 fun Note(){
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(rwGreen)
-        )
+    val backgroundShape: Shape = RoundedCornerShape(4.dp)
+    Row(modifier = Modifier
+        .padding(8.dp)
+        .shadow(1.dp, backgroundShape)
+        .fillMaxWidth()
+        .heightIn(min = 64.dp)
+        .background(Color.White, backgroundShape)
+
+    ) {
+        NoteColor(rwGreen, 40.dp, 4.dp, 1.dp)
+
         Column(modifier = Modifier.weight(1f)) {
-            Text(text = "Заголовок", maxLines = 1)
-            Text(text = "Содержание", maxLines = 1)
+            Text("Заголовок", maxLines = 1)
+            Text("Содержание", maxLines = 1)
         }
 
         Checkbox(
             checked = false, onCheckedChange = {},
             modifier = Modifier.padding(start = 8.dp)
         )
-
     }
-
 }
 
 
